172ae8b43caff8fc4aeef6828a209b0dd82cfc8c
Jonathan McCoy <jonathan@perceptionlabs.com>
2011-09-12 19:52:15 -0700

    Initial commit
    rails 3.1 app, no database

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RdsBrowser::Application.config.secret_token = '906a3e299157945a25d8bfab3f0d01f91d0cd22b26e801700571e80150ced07698fadef26855920ef1aaaf45c6a348d2f6cc7d79af59da568b5de2b9fa4f8643'
